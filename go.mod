2f92b46e1c1832c9a6b1fe20faee2b6d16de9b90
sandman <sandmanhome@hotmail.com>
2020-07-17 10:40:55 +0800

    use ipfs api

--- go.mod
+++ go.mod
@@ -3,23 +3,19 @@ module server
 go 1.14
 
 require (
-	github.com/libp2p/go-conn-security-multistream v0.2.0
+	github.com/ipfs/go-datastore v0.4.4
+	github.com/ipfs/go-ipfs v0.5.3
+	github.com/ipfs/go-ipfs-config v0.5.3
 	github.com/libp2p/go-libp2p v0.8.3
+	github.com/libp2p/go-libp2p-circuit v0.2.2
 	github.com/libp2p/go-libp2p-core v0.5.3
+	github.com/libp2p/go-libp2p-peer v0.2.0
 	github.com/libp2p/go-libp2p-peerstore v0.2.3
-	github.com/libp2p/go-libp2p-pubsub v0.2.7
-	github.com/libp2p/go-libp2p-secio v0.2.2
-	github.com/libp2p/go-libp2p-swarm v0.2.3
-	github.com/libp2p/go-libp2p-transport-upgrader v0.2.0
-	github.com/libp2p/go-libp2p-yamux v0.2.7
-	github.com/libp2p/go-stream-muxer-multistream v0.3.0
-	github.com/libp2p/go-tcp-transport v0.2.0
-	github.com/multiformats/go-multiaddr v0.2.1
-
 )
 
 replace (
 	github.com/btcsuite/btcd v0.20.1-beta => gitlab.com/ihyperdata/icfs/btcd.git v0.1.0
+	github.com/ipfs/go-ipfs v0.5.3 => ../icfs/go-ipfs
 	github.com/ipfs/go-ipfs-config v0.5.3 => gitlab.com/ihyperdata/icfs/go-ipfs-config.git v0.2.0
 	github.com/ipfs/go-verifcid v0.0.1 => gitlab.com/ihyperdata/icfs/go-verifcid.git v0.0.0-20200616091530-02d96dec2b07
 	github.com/libp2p/go-libp2p v0.8.3 => gitlab.com/ihyperdata/icfs/go-libp2p.git v0.1.0
--- main.go
+++ main.go
@@ -3,170 +3,142 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
+	"time"
 
-	csms "github.com/libp2p/go-conn-security-multistream"
-	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/metrics"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/ipfs/go-datastore"
+	syncds "github.com/ipfs/go-datastore/sync"
+	config "github.com/ipfs/go-ipfs-config"
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/node/libp2p"
+	"github.com/ipfs/go-ipfs/repo"
+	libp2p1 "github.com/libp2p/go-libp2p"
+	relay "github.com/libp2p/go-libp2p-circuit"
+	host1 "github.com/libp2p/go-libp2p-core/host"
+	peer "github.com/libp2p/go-libp2p-peer"
 	peerstore "github.com/libp2p/go-libp2p-peerstore"
-	pstoremem "github.com/libp2p/go-libp2p-peerstore/pstoremem"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	secio "github.com/libp2p/go-libp2p-secio"
-	swarm "github.com/libp2p/go-libp2p-swarm"
-	tptu "github.com/libp2p/go-libp2p-transport-upgrader"
-	yamux "github.com/libp2p/go-libp2p-yamux"
-	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
-	msmux "github.com/libp2p/go-stream-muxer-multistream"
-	"github.com/libp2p/go-tcp-transport"
-
-	"github.com/multiformats/go-multiaddr"
 )
 
+// printSwarmAddrs prints the addresses of the host
+func printSwarmAddrs(node *core.IpfsNode) {
+	if !node.IsOnline {
+		fmt.Println("Swarm not listening, running in offline mode.")
+		return
+	}
+
+	var lisAddrs []string
+	ifaceAddrs, err := node.PeerHost.Network().InterfaceListenAddresses()
+	if err != nil {
+		fmt.Printf("failed to read listening addresses: %s", err)
+	}
+	for _, addr := range ifaceAddrs {
+		lisAddrs = append(lisAddrs, addr.String())
+	}
+	sort.Strings(lisAddrs)
+	for _, addr := range lisAddrs {
+		fmt.Printf("Swarm listening on %s\n", addr)
+	}
+
+	var addrs []string
+	for _, addr := range node.PeerHost.Addrs() {
+		addrs = append(addrs, addr.String())
+	}
+	sort.Strings(addrs)
+	for _, addr := range addrs {
+		fmt.Printf("Swarm announcing %s\n", addr)
+	}
+
+}
+
+// isolates the complex initialization steps
+func constructPeerHost(ctx context.Context, id peer.ID, ps peerstore.Peerstore, options ...libp2p1.Option) (host1.Host, error) {
+	pkey := ps.PrivKey(id)
+	if pkey == nil {
+		return nil, fmt.Errorf("missing private key for node ID: %s", id.Pretty())
+	}
+	relayOpts := []relay.RelayOpt{}
+	options = append([]libp2p1.Option{libp2p1.Identity(pkey), libp2p1.Peerstore(ps), libp2p1.EnableRelay(relayOpts...), libp2p1.EnableAutoRelay(), libp2p1.DefaultStaticRelays()}, options...)
+	return libp2p1.New(ctx, options...)
+}
+
 func main() {
-	// The context governs the lifetime of the libp2p node.
-	// Cancelling it will stop the the host.
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// Now, normally you do not just want a simple host, you want
-	// that is fully configured to best support your p2p application.
-	// Let's create a second host setting some more options.
-
-	// Set your own keypair
-	priv, _, err := crypto.GenerateKeyPair(
-		crypto.Sm2p256v1, // Select your key type. Ed25519 are nice short
-		-1,               // Select key length when possible (i.e. RSA).
-	)
+	ds := syncds.MutexWrap(datastore.NewMapDatastore())
+	cfg, err := config.Init(ioutil.Discard, 2048)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 
-	//psk := []byte{0x55, 0x48, 0xb7, 0xf2, 0xeb, 0xd9, 0x56, 0x80, 0x81, 0xab, 0x23, 0xa6, 0x1f, 0x15, 0xff, 0x68, 0x24, 0xe8, 0x35, 0xe5, 0xca, 0xc4, 0xc5, 0x1c, 0xaf, 0x27, 0xd8, 0xbc, 0x5c, 0x3e, 0x6c, 0x4d}
+	cfg.Datastore = config.Datastore{}
 
-	// h, err := libp2p.New(ctx,
-	// 	// Use the keypair we generated
-	// 	libp2p.Identity(priv),
-	// 	// Multiple listen addresses
-	// 	libp2p.ListenAddrStrings(
-	// 		"/ip4/0.0.0.0/tcp/9392", // regular tcp connections
-	// 	),
-	// 	libp2p.PrivateNetwork(psk),
-	// )
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	pid, err := peer.IDFromPublicKey(priv.GetPublic())
-	if err != nil {
-		panic(err)
-	}
-
-	pstore := pstoremem.NewPeerstore()
-
-	if err := pstore.AddPrivKey(pid, priv); err != nil {
-		panic(err)
-	}
-	if err := pstore.AddPubKey(pid, priv.GetPublic()); err != nil {
-		panic(err)
-	}
-
-	s := swarm.NewSwarm(ctx, pid, pstore, metrics.NewBandwidthCounter())
-
-	id := s.LocalPeer()
-	pk := s.Peerstore().PrivKey(id)
-	secMuxer := new(csms.SSMuxer)
-	secMuxer.AddTransport(secio.ID, &secio.Transport{
-		LocalID:    id,
-		PrivateKey: pk,
+	nd, err := core.NewNode(ctx, &core.BuildCfg{
+		Permanent: true,
+		Online:    true,
+		Routing:   libp2p.DHTOption,
+		ExtraOpts: map[string]bool{
+			"pubsub": true,
+		},
+		Repo: &repo.Mock{
+			C: *cfg,
+			D: ds,
+		},
+		Host: constructPeerHost,
 	})
 
-	stMuxer := msmux.NewBlankTransport()
-	stMuxer.AddTransport("/yamux/1.0.0", yamux.DefaultTransport)
-
-	upgrader := &tptu.Upgrader{
-		Secure:  secMuxer,
-		Muxer:   stMuxer,
-		Filters: s.Filters,
-	}
-
-	//upgrader.ConnGater = cfg.connectionGater
-	tcpTransport := tcp.NewTCPTransport(upgrader)
-	//tcpTransport.DisableReuseport = cfg.disableReuseport
-
-	if err := s.AddTransport(tcpTransport); err != nil {
-		panic(err)
-	}
-
-	var ZeroLocalTCPAddress, _ = multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/9000")
-	if err := s.Listen(ZeroLocalTCPAddress); err != nil {
-		panic(err)
-	}
-
-	s.Peerstore().AddAddrs(pid, s.ListenAddresses(), peerstore.PermanentAddrTTL)
-
-	bh := bhost.New(s)
-
-	fmt.Printf("Hello World, my second hosts ID is %s\n", bh.ID())
-
-	// The last step to get fully up and running would be to connect to
-	// bootstrap peers (or any other peers). We leave this commented as
-	// this is an example and the peer will die as soon as it finishes, so
-	// it is unnecessary to put strain on the network.
-
-	var DefaultBootstrapPeers []multiaddr.Multiaddr
-	for _, s := range []string{
-		//"/dns4/public.baasze.com/tcp/4001/p2p/bafzm3jqbebirvt7q5o2o35kgxr2vskstfvxhzztukp7xfhgd3vjkfexaznujy",
-		//"/ip4/127.0.0.1/tcp/9000/p2p/bafzm3jqbedqdpe5giegij4oiiczwupkk4cqlh3ufjvx3tzwchbhshilm3p7ci",
-	} {
-		ma, err := multiaddr.NewMultiaddr(s)
-		if err != nil {
-			panic(err)
-		}
-		DefaultBootstrapPeers = append(DefaultBootstrapPeers, ma)
-	}
-
-	// This connects to public bootstrappers
-	//for _, addr := range dht.DefaultBootstrapPeers {
-	for _, addr := range DefaultBootstrapPeers {
-		pi, _ := peer.AddrInfoFromP2pAddr(addr)
-		// We ignore errors as some bootstrap peers may be down
-		// and that is fine.
-		err := bh.Connect(ctx, *pi)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Printf("success connect %s\n", addr.String())
-		}
-	}
-
-	fmt.Printf("peers: %v\n", bh.Peerstore().Peers())
-
-	var pubsubOptions []pubsub.Option
-	pubsubOptions = append(
-		pubsubOptions,
-		pubsub.WithMessageSigning(true),
-		pubsub.WithStrictSignatureVerification(false),
-	)
-	ps, err := pubsub.NewGossipSub(ctx, bh, pubsubOptions...)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	sub, err := ps.Subscribe("fuck")
+	nd.IsDaemon = true
 
+	fmt.Println(nd.PeerHost.Addrs())
+	fmt.Println(nd.Identity.Pretty())
+	fmt.Println(nd.Peerstore.Peers())
+	printSwarmAddrs(nd)
+
+	pubsub := nd.PubSub
+	// sub, err := pubsub.Subscribe("fuck")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	ticker := time.NewTicker(time.Second * 1)
 	for {
-		got, err := sub.Next(ctx)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(got.GetData())
+		// got, err := sub.Next(ctx)
+		// if err != nil {
+		// 	fmt.Println(err)
+		// } else {
+		// 	fmt.Println(got.GetData())
+		// }
+		select {
+		case <-ticker.C:
+			fmt.Println(nd.PeerHost.Addrs())
+			pubsub.Publish("fuck", []byte("fuck fuck"))
 		}
 
-		select {
-		case <-ctx.Done():
-			break
-		default:
-		}
 	}
 
+	select {}
+
+	// cctx := commands.Context{
+	// 	Online: true,
+	// 	ConstructNode: func() (*core.IpfsNode, error) {
+	// 		return nd, nil
+	// 	},
+	// }
+
+	// list, err := net.Listen("tcp", ":0")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// log.Println("listening on: ", list.Addr())
+
+	// if err := corehttp.Serve(nd, list, corehttp.CommandsOption(cctx)); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
